fe4b7cd467561dbf3107ac6282c76520f2891d56
sga2022 <sga@kitech.re.kr>
2022-12-21 00:12:25 +0900

    update temperature influence

--- isofit_diffT_test.py
+++ isofit_diffT_test.py
@@ -47,7 +47,7 @@ for T in T_dum:
 # Draw a graph
 # %%
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(dpi = 100)
 
 lines = ('-','--','-.',':', (0,(3,1,0.5,1,0.5)))
 for qq_pr, li in zip(q_pred, lines): 
@@ -59,8 +59,9 @@ for qq_pr, li in zip(q_pred, lines):
 markers = ('o','^','D','v','p','sq')
 for ma, qq,  T  in zip(markers, q_dum, T_dum):
     ax.plot(P_dum, qq, ma, markersize = 7.5,
+            markeredgewidth = 1.5,
             label = 'T = {0:.1f} K'.format(T),
-            mfc = 'w', mec = 'k')
+            mfc = 'None', mec = 'k')
     
 ax.legend(fontsize = 13)
 
@@ -68,6 +69,52 @@ ax.legend(fontsize = 13)
 ax.set_xlabel('pressure (bar)', fontsize = 13)
 ax.set_ylabel('uptake (mol/kg)', fontsize = 13)
 ax.grid(which='major', linestyle = ':')
-fig.savefig('fig_test_T.png')
+fig.savefig('fig_test_T.png', bbox_inches = 'tight')
+
+
+# %% 
+# Graph: raw data
+# %%
+fig2, ax2 = plt.subplots(dpi=100)
+fsize = 16.5
+markers = ('o','^','D','v','p','sq')
+for ma, qq,  T  in zip(markers, q_dum, T_dum):
+    ax2.plot(P_dum, qq, ma, markersize = 7.5,
+             markeredgewidth = 1.5,
+             label = 'T = {0:.1f} K'.format(T),
+             mfc = 'w', mec = 'k')
+    
+ax2.legend(fontsize = fsize)
+
+ax2.set_xlabel('pressure (P: bar)', fontsize = fsize)
+ax2.set_ylabel('uptake (q: mol/kg)', fontsize = fsize)
+ax2.tick_params(labelsize = fsize-2)
+ax2.grid(which='major', linestyle = ':')
+fig2.savefig('qP_raw.png', bbox_inches='tight')
+
+# %%
+# Graph: normalized data
+# %%
+P_norm_list = []
+
+for thet in theta_list:
+    P_tmp = P_dum*thet
+    P_norm_list.append(P_tmp)
+    
+fig3, ax3 = plt.subplots(dpi = 100)
+
+for pp, qq, ma, T in zip(P_norm_list, q_dum, markers, T_dum):
+    ax3.plot(pp,qq, ma, markersize = 7.5,
+             markeredgewidth = 1.5,
+             label = 'T = {0:.1f} K'.format(T),
+             mfc = 'None', mec = 'k')
+ax3.legend(fontsize = fsize)
+ax3.set_xlabel('normalized pressure (P$_{norm}$: -)',
+               fontsize = fsize)
+ax3.tick_params(labelsize = fsize-2)
+ax3.set_ylabel('uptake (q: mol/kg)',
+               fontsize = fsize)
+ax3.grid(which='major', linestyle = ':')
+fig3.savefig('qP_normal.png',bbox_inches = 'tight')
 
 
